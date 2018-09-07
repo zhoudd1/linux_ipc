c36cda5e23ecafffc4102b4e4adc63a9898dbf69
zhoudd1 <15019442511@163.com>
2018-09-07 10:58:57 +0800

    Add files via upload

--- msg_queue/recv.c
+++ msg_queue/recv.c
@@ -1,25 +1,47 @@
-#include <sys/types.h>
-#include <sys/msg.h>
-#include <sys/ipc.h>
-#include <string.h>
 #include <stdio.h>
- 
-int main (void) {
- 
-        key_t ipckey;
-        int mq_id;
-        struct { long type; char text[100]; } mymsg;
-        int received;
- 
-        /* Generate the ipc key */
-        ipckey = ftok("/tmp/foo", 42);
-        printf("My key is %d\n", ipckey);
- 
-        /* Set up the message queue */
-        mq_id = msgget(ipckey, 0);
-        printf("Message identifier is %d\n", mq_id);
- 
-        received = msgrcv(mq_id, &mymsg, sizeof(mymsg), 0, 0);
- 
-        printf("%s (%d)\n", mymsg.text, received);
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <errno.h>
+
+#define MSG_LEN     100
+#define MSG_KEY    1234
+
+//msg_queue
+typedef struct 
+{
+    long int type; 
+    char data[MSG_LEN];   
+}msg_t;
+
+static int msgid;
+
+//msg queue init
+int msg_queue_creat(key_t key)
+{   
+	msgid=msgget(key,0666 | IPC_CREAT);
+	if(msgid==-1)
+	{
+	    fprintf(stderr,"msgget failed with error: %d\n",errno);		
+	}
+    return 0;
+}
+
+int main()
+{    
+    msg_queue_creat(MSG_KEY);
+
+    while(1){
+
+        msg_t msg; 
+        if(msgrcv(msgid, &msg, sizeof(msg_t), 0, IPC_NOWAIT) == -1)
+	    {
+	        //fprintf(stderr,"msgrcv failed with error: %d\n",errno);
+	    }
+        else printf("recv a message: %ld,%s\n",msg.type,msg.data);
+
+        sleep(1);
+    }
 }
--- msg_queue/send.c
+++ msg_queue/send.c
@@ -1,27 +1,45 @@
-#include <sys/types.h>
-#include <sys/msg.h>
-#include <sys/ipc.h>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <errno.h>
+
+#define MSG_LEN     100
+#define MSG_KEY    1234
+
+//msg_queue
+typedef struct 
+{
+    long int type; 
+    char data[MSG_LEN];   
+}msg_t;
+
+static int msgid;
+
+//msg queue init
+int msg_queue_creat(key_t key)
+{   
+	msgid=msgget(key,0666 | IPC_CREAT);
+	if(msgid==-1)
+	{
+	    fprintf(stderr,"msgget failed with error: %d\n",errno);		
+	}
+    return 0;
+}
+
+int main()
+{
+    msg_queue_creat(MSG_KEY);
  
-int main (void) {
- 
-        key_t ipckey;
-        int mq_id;
-        struct { long type; char text[100]; } mymsg;
- 
-        /* Generate the ipc key */
-        ipckey = ftok("/tmp/foo", 42);
-        printf("My key is %d\n", ipckey);
- 
-        /* Set up the message queue */
-        mq_id = msgget(ipckey, IPC_CREAT | 0666);
-        printf("Message identifier is %d\n", mq_id);
- 
-        /* Send a message */
-        memset(mymsg.text, 0, 100); /* Clear out the space */
-        strcpy(mymsg.text, "Hello, world!");
-        mymsg.type = 1;
-        msgsnd(mq_id, &mymsg, sizeof(mymsg), 0);
- 
+    while(1) {  
+        msg_t msg={1,"hello,world"};
+        if(msgsnd(msgid, &msg, sizeof(msg_t), IPC_NOWAIT) == -1)
+	    {
+	        //fprintf(stderr,"msgsnd failed with error: %d\n",errno);
+	    }
+        else printf("send a message\n");        
+        sleep(2);
+    }
 }
